48ee1e14cd1c83f380b6aa8b00ac104dd4b17d6c
Eddie Y <eyasi@acadian-asset.com>
2018-08-29 09:46:04 -0400

    Example use of site.css and image

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import {WeatherTable} from "./weather-table";
+import { Image } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 
 interface ISemanticUiThemingState {
     currentThemeInfo: string;
@@ -16,9 +18,15 @@ export class ThemingPage extends React.Component<IThemingPageProps, ISemanticUiT
             currentThemeInfo: "none",
         };
     }
+    private acadianLogoSource: string = "/src/resources/AcadianLogo.gif";
+
     public render() {
         return(
                 <div>
+                    <div id="appHeader">
+                    <Image src={this.acadianLogoSource} floated="left" verticalAlign="middle" className="appHeaderImage" />
+                    <Header as="h1" inverted textAlign="right" verticalAlign="middle">Hourly Forecasts</Header>
+                    </div>
                     <h2 className="ui header">Boston's Weather Forecast</h2>
                     <WeatherTable tableTitle="Boston's Hourly Forecast" initialRowsPerPage={5} />
                 </div>
